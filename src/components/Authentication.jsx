e4c573582edc77dd49bef898f989dc11bab3a911
EduardoGC715 <99993140+EduardoGC715@users.noreply.github.com>
2024-05-12 17:49:45 -0600

    authentication verification

---
+++ src/components/Authentication.jsx
@@ -0,0 +1,25 @@
+
+import React, { useEffect } from 'react';
+
+function AuthenticationChecker() {
+    useEffect(() => {
+        // Check if loggedIn
+        try {
+            const value = window.localStorage.getItem("USER_ID");
+            if (value === null) {
+                window.location.href = '/login';
+            }
+        } catch (e) {
+            console.error("Error checking authentication:", e);
+            window.location.href = '/login';
+        }
+    }, []); // Empty dependency array ensures this effect runs only once on component mount
+
+    return (
+        <div>
+            {/* You can render any loading indicator or other content here */}
+        </div>
+    );
+}
+
+export default AuthenticationChecker;
